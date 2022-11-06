555630c77536722ac4faf7f61a2543012b4c5684
Invincible-21 <shantobd1969@gmail.com>
2022-11-06 07:40:36 +0600

    miles to go

--- CPT_Coding Ninjas/4.Dynamic Allocation/test.cpp
+++ CPT_Coding Ninjas/4.Dynamic Allocation/test.cpp
@@ -1,1 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int getValue(int x=0, int y=0, int z){
+    return (x+y+z);
+}
+
+int main()
+{
+    cout<<getValue(10);
+
+    return 0;
+}
+// Compilation Error --> Default parameters must be added last in C++ functions
