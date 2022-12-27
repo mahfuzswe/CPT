7b5f11285cc9139ecfad41840e28fdd2f1ce9e03
Invincible-21 <shantobd1969@gmail.com>
2022-12-27 21:59:17 +0600

    updated

--- CPT_Coding Ninjas/5.Recursion/test.cpp
+++ CPT_Coding Ninjas/5.Recursion/test.cpp
@@ -2,22 +2,17 @@
 using namespace std;
 
 int factorial(int n){
-    if(n==0){
-        return 1;
-    }
+    if(n==0) return 1;
+
     int smallOutput = factorial(n-1);
-    int output = n*smallOutput;
+    int output = n * smallOutput;
     return output;
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    
-    cout<<factorial(4)<<endl;
-    
-    
+
+int main(){
+
+    cout<<factorial(3)<<endl;
+
     return 0;
 }
-
