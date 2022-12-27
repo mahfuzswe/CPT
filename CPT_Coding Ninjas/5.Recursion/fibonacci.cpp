14586d5f30ab5333b7338a731d7df7cba3d4acad
Invincible-21 <shantobd1969@gmail.com>
2022-12-27 23:52:53 +0600

    updated

--- CPT_Coding Ninjas/5.Recursion/fibonacci.cpp
+++ CPT_Coding Ninjas/5.Recursion/fibonacci.cpp
@@ -2,14 +2,13 @@
 using namespace std;
 
 int fib(int n){
-
     if(n==0) return 0;
     if(n==1) return 1;
-    
+
     int smallOutput1 = fib(n-1);
     int smallOutput2 = fib(n-2);
 
-    return smallOutput1 + smallOutput2;
+    return smallOutput1+smallOutput2;
 }
 
 int main()
@@ -18,7 +17,7 @@ int main()
     cin.tie(0);
     
     int n; cin>>n;
-    
+
     cout<<fib(n)<<endl;
     
     return 0;
